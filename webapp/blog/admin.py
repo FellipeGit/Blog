6b8d78b0fd0133841936b43f55d05dcc81744693
FellipeGit <thenuuck@gmail.com>
2022-04-09 18:43:03 -0300

    create a post model and your view

--- webapp/blog/admin.py
+++ webapp/blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'slug', 'author', 'created_date', 'updated_date', 'status')
+    list_filter = ('author', 'slug', 'created_date', 'updated_date', 'status')
+    search_fields = ['title', 'slug']
+    prepopulated_fields = {'slug': 'title'}
+
+admin.site.register(Post, PostAdmin)
--- webapp/blog/models.py
+++ webapp/blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create models here
+STATUS = (
+    (0, "Writing"),
+    (1, "Publish")
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='post')
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.title
